38567b155644d9c27d0026ac2d2693b9f09ba898
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2022-03-24 19:17:23 -0400

    Proyecto de Bot para Recursos Humanos

---
+++ src/main/java/bo/edu/ucb/ingsoft/hhrr/HhrrApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.hhrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/ingsoft/hhrr/HhrrApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.hhrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
